81e2f7360d87b2aef3e2f44d17803bd6216ba69b
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2021-12-09 11:50:50 -0800

    Add ResponseStatus(500) annotation

--- src/main/kotlin/com/jtaylorsoftware/livequiz/api/feedback/service/ServiceException.kt
+++ src/main/kotlin/com/jtaylorsoftware/livequiz/api/feedback/service/ServiceException.kt
@@ -1,5 +1,9 @@
 package com.jtaylorsoftware.livequiz.api.feedback.service
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 class ServiceException : RuntimeException {
     constructor(): super()
     constructor(throwable: Throwable) : super(throwable)
